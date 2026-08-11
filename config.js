2091929353d785bf34675f479e04c2f67a5d2637
vihangabot44 <prarthanasanjeewani82@gmail.com>
2026-08-11 08:34:42 +0000

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 VIHANGA-MD Is Alive Now😍*",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/vihangabot44/vihanga_md_bot/blob/main/images/file_000000006f7c820dafaebee302cc4607.png?raw=true",
+ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 VIHANGA-MD BOT Is Alive Now😍*",
 BOT_OWNER: '94786053450',  // Replace with the owner's phone number
 
 
